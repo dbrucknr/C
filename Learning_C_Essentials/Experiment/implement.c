686936839a04435141e993675436d381af5197ba
Derek Bruckner <derek.bruckner@detroitlabs.com>
2022-01-21 08:35:50 -0500

    Add foundation for course challenge.

---
+++ Learning_C_Essentials/Experiment/implement.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    The main function generates multiple lines of output. 
+    Performs the following steps:
+        1. Get and verify input
+        2. To avoid out-of-range-values
+        3. Process rows
+*/
+int main()
+{
+    return 0;
+}
